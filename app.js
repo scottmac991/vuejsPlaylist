392ced6710c525acdb213dd0ad1d79a0b75295eb
Scott <Scott@iMac.local>
2019-02-24 23:03:48 -0800

    lesson 5

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ new Vue({
   data: {
     name: 'Scott',
     job: 'Ninja',
-    website: 'http://www.thenetninja.com'
-  },
+    website: 'http://www.thenetninja.com.uk',
+    websiteTag: '<a href="http://www.thenetninja.com.uk">The Website</a>',
+    age: 25,
+    x: 0,
+    y: 0
+    },
 
   methods: {
     greet: function(time) {
       return 'Good ' + time + ' ' + this.name;
+    },
+
+    addAge: function(years_to_add) {
+      console.log("In vue-app::addAge()")
+      this.age += years_to_add;
+    },
+
+    subtractAge: function(years_to_subtract) {
+      console.log("In vue-app::subtractAge()")
+      this.age -= years_to_subtract;
+    },
+
+    updateXY: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
